6589f3f91311d1baca58eff22856e3e5760b4358
Maurice Cantaert <maurice.cantaert@student.hogent.be>
2023-05-21 20:57:40 +0200

    initial commit

---
+++ src/test/java/be/mauricecantaert/ewdjproject/EwdjProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package be.mauricecantaert.ewdjproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EwdjProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
