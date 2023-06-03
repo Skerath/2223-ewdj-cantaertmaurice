239a898c04717e1708607ab41a6f387a8a22ff92
Maurice Cantaert <maurice.cantaert@student.hogent.be>
2023-06-03 15:47:43 +0200

    bugfixes

--- src/main/java/Aalst_Cantaert_Maurice/SecurityConfig.java
+++ src/main/java/Aalst_Cantaert_Maurice/SecurityConfig.java
@@ -31,7 +31,7 @@ public class SecurityConfig {
                         requests.requestMatchers("/login**").permitAll()
                                 .requestMatchers("/css/**").permitAll()
                                 .requestMatchers("/403**").permitAll()
-                                .requestMatchers("/book/**").permitAll() //TODO check
+                                .requestMatchers("/book/**").permitAll()
                                 .requestMatchers("/registerbook")
                                 .access(new WebExpressionAuthorizationManager("hasRole('ROLE_ADMIN')"))
                                 .requestMatchers("/update/*")
